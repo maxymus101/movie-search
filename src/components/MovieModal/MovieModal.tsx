27c38822e85de51e66648bbe6d671544e06ce455
Max Akimov <maxymus2828@gmail.com>
2026-01-28 12:27:26 +0200

    added logic for Modal close

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom";
 import type { Movie } from "../../types/movies";
 import css from "./MovieModal.module.css";
+import { useEffect } from "react";
 
 interface MovieModalProps {
   movie: Movie | null;
@@ -8,10 +9,36 @@ interface MovieModalProps {
 }
 
 export default function MovieModal({ movie, onClose }: MovieModalProps) {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  });
+
+  const handleBackDropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
   if (!movie) return null;
   const { title, backdrop_path, overview, release_date, vote_average } = movie;
+
   return createPortal(
-    <div className={css.backdrop} role="dialog" aria-modal="true">
+    <div
+      className={css.backdrop}
+      onClick={handleBackDropClick}
+      role="dialog"
+      aria-modal="true"
+    >
       <div className={css.modal}>
         <button
           className={css.closeButton}
